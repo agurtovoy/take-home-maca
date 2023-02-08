977904bfdb0895384c0eb3e3887c9501f1a0f11c
Aleksey Gurtovoy <agurtovoy@acm.org>
2023-02-08 08:18:32 -0600

    PokeAPI's GraphQL endpoint is back online, switch back to using it

--- src/lib/api/poke-api.js
+++ src/lib/api/poke-api.js
@@ -1,10 +1,10 @@
 import useSWRImmutable from "swr/immutable";
 import useSWRInfinite from "swr/infinite";
 
-// import { fetcher, queries, fetchPokemons, PAGE_SIZE } from "@/lib/api/graphql-api";
-// export { queries } from "@/lib/api/graphql-api";
-import { fetcher, queries, fetchPokemons, PAGE_SIZE } from "@/lib/api/rest-api";
-export { queries } from "@/lib/api/rest-api";
+import { fetcher, queries, fetchPokemons, PAGE_SIZE } from "@/lib/api/graphql-api";
+export { queries } from "@/lib/api/graphql-api";
+// import { fetcher, queries, fetchPokemons, PAGE_SIZE } from "@/lib/api/rest-api";
+// export { queries } from "@/lib/api/rest-api";
 
 export const usePokeAPI = (url, options) => useSWRImmutable(url, fetcher, options);
 export const usePokeAPIInfinite = (getKey, options) =>
